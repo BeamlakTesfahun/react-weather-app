5920b63ae96da02edfdd5af0ecc987fdcc4a6c4e
Beamlak Tesfahun <beamlakt24@gmail.com>
2024-12-25 12:12:18 +0300

    included API call and state management logic

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,62 +1,96 @@
 import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
 
-export default function Body() {
-  const weatherIcons = {
-    Mon: "CLEAR_DAY",
-    Tue: "PARTLY_CLOUDY_DAY",
-    Wed: "CLOUDY",
-    Thu: "RAIN",
-    Fri: "SNOW",
+export default function Body({ weatherData, forecastData }) {
+  // const weatherIcons = {
+  //   Mon: "CLEAR_DAY",
+  //   Tue: "PARTLY_CLOUDY_DAY",
+  //   Wed: "CLOUDY",
+  //   Thu: "RAIN",
+  //   Fri: "SNOW",
+  // };
+
+  // for formatting current date and time
+  const getCurrentDateTime = () => {
+    const now = new Date();
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const day = days[now.getDay()];
+    const hours = now.getHours();
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const ampm = hours >= 12 ? "PM" : "AM";
+    const formattedTime = `${day}, ${hours % 12 || 12}:${minutes} ${ampm}`;
+    return formattedTime;
   };
   return (
     <>
-      <div className="weather-app-parameters main">
-        <div>
-          <h1 className="weather-app-city">Addis Ababa</h1>
-          <p className="weather-app-data">
-            <span>Monday, 12:30 PM</span>
-            <span>, </span>
-            <span className="description">Partly Cloudy</span>
-            <br />
-            <span> Humidity: </span>
-            <strong className="humidity">65</strong>
-            <strong>%</strong>,<span> Wind: </span>
-            <strong className="wind-speed">15</strong>
-            <strong> km/h</strong>
-          </p>
-        </div>
-        <div className="weather-app-data-container">
-          <div className="weather-app-emoji">
-            <ReactAnimatedWeather
-              icon="CLEAR_DAY"
-              color="black"
-              size={60}
-              animate={true}
-            />
-            {/* <img
+      {weatherData && weatherData.city && (
+        <div className="weather-app-parameters main">
+          <div>
+            <h1 className="weather-app-city">{weatherData.city}</h1>
+            <p className="weather-app-data">
+              <span>{getCurrentDateTime()}</span>
+              <span>, </span>
+              <span className="description">{weatherData.description}</span>
+              <br />
+              <span> Humidity: </span>
+              <strong className="humidity">{weatherData.humidity}</strong>
+              <strong>%</strong>,<span> Wind: </span>
+              <strong className="wind-speed">{weatherData.wind}</strong>
+              <strong> km/h</strong>
+            </p>
+          </div>
+          <div className="weather-app-data-container">
+            <div className="weather-app-emoji">
+              <img
+                src={`http://openweathermap.org/img/wn/${weatherData.icon}.png`}
+                alt={weatherData.description}
+                className="weather-app-emoji"
+              />
+              {/* <ReactAnimatedWeather
+                icon="CLEAR_DAY"
+                color="black"
+                size={60}
+                animate={true}
+              /> */}
+              {/* <img
               src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
               alt="Cloudy Icon"
               className="weather-app-emoji"
             /> */}
+            </div>
+            <div className="weather-app-temp">{weatherData.temperature}</div>
+            <div className="weather-app-unit">°C</div>
           </div>
-          <div className="weather-app-temp">22</div>
-          <div className="weather-app-unit">°C</div>
         </div>
-      </div>
-
+      )}
       {/* weather Forecast section */}
       <div className="weather-forecast">
-        {["Mon", "Tue", "Wed", "Thu", "Fri"].map((day) => (
-          <div key={day}>
-            <p className="weather-forecast-date">{day}</p>
+        {forecastData.map((day, index) => (
+          <div key={index}>
+            <p className="weather-forecast-date">
+              {new Date(day.dt_txt).toLocaleDateString("en-US", {
+                weekday: "long",
+              })}
+            </p>
             <div className="weather-forecast-icon">
-              <ReactAnimatedWeather
+              <img
+                src={`http://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
+                alt={day.weather[0].description}
+              />
+              {/* <ReactAnimatedWeather
                 icon={weatherIcons[day]}
                 color="black"
                 size={60}
                 animate={true}
-              />
+              /> */}
             </div>
             {/* <img
               src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
@@ -64,8 +98,12 @@ export default function Body() {
               className="weather-forecast-icon"
             /> */}
             <div className="weather-forecast-temperatures">
-              <span className="weather-forecast-temperature">15°C</span>
-              <span className="weather-forecast-temperature">30°C</span>
+              <span className="weather-forecast-temperature">
+                {Math.round(day.main.temp_min)}°C
+              </span>
+              <span className="weather-forecast-temperature">
+                {Math.round(day.main.temp_max)}°C
+              </span>
             </div>
           </div>
         ))}
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,79 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import Body from "./Body";
+import axios from "axios";
 
 export default function Header() {
+  const inputRef = useRef();
+  const [weather, setWeather] = useState({});
+  const [forecast, setForecast] = useState([]);
+
+  const getForecast = (response) => {
+    //console.log(response.data.main);
+    setWeather({
+      city: inputRef.current.value || "Addis Ababa",
+      temperature: Math.round(response.data.main?.temp),
+      description: response.data.weather[0].description,
+      humidity: response.data.main?.humidity,
+      wind: response.data.wind?.speed,
+      icon: response.data.weather[0].icon,
+    });
+    // fetch forecast data
+    const city = inputRef.current.value || "Addis Ababa";
+    const apiKey = "2513f3c728b1b5ff4f4347e1a6af22b8";
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios
+      .get(forecastUrl)
+      .then((forecastResponse) => {
+        const dailyData = forecastResponse.data.list.filter((item) =>
+          item.dt_txt.includes("12:00:00")
+        );
+        setForecast(dailyData);
+      })
+      .catch((error) => {
+        console.error("Error fetching forecast data", error);
+      });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setWeather({ city: "Addis Ababa" });
+    const city = inputRef.current.value;
+    if (city) {
+      let apiKey = "2513f3c728b1b5ff4f4347e1a6af22b8";
+      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+      axios
+        .get(apiUrl)
+        .then(getForecast)
+        .catch((error) => {
+          console.error("Error occured while fetching weather data");
+        });
+    }
+  };
+
+  // fetch weather data for Addis Ababa on initial render
+  useEffect(() => {
+    const defaultCity = "Addis Ababa";
+    let apiKey = "2513f3c728b1b5ff4f4347e1a6af22b8";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${defaultCity}&appid=${apiKey}&units=metric`;
+    axios
+      .get(apiUrl)
+      .then(getForecast)
+      .catch((error) => {
+        console.error(
+          "Error occurred while fetching weather data for default city",
+          error
+        );
+      });
+  }, []);
+
   return (
     <div className="header">
-      <form className="search-form" id="search-form-id">
+      <form className="search-form" id="search-form-id" onSubmit={handleSubmit}>
         <input
           type="search"
-          placeholder="Enter a city"
+          placeholder="Enter a city.."
+          ref={inputRef}
           required
           className="search-form-input"
           id="search-form-input-id"
@@ -17,6 +84,7 @@ export default function Header() {
           className="search-form-button"
         />
       </form>
+      <Body weatherData={weather} forecastData={forecast} />
     </div>
   );
 }
